eb5780ab590e0c82d46d4c555cce70f4b533f472
DrackPrez <nicoperezperez0@gmail.com>
2025-07-03 16:47:06 -0400

    Update database configuration to use dj-database-url for Railway integration

--- project_anfa/settings.py
+++ project_anfa/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url # Necesitas este import
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,21 +86,12 @@ WSGI_APPLICATION = 'project_anfa.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Obtiene la URL de la base de datos de Railway
-# Railway inyecta esta variable de entorno automáticamente
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    # Usa dj_database_url para analizar la URL y configurar la base de datos
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600  # Mantener conexiones activas por 10 minutos
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
     )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
