c90a2915ed2569a8df426e5a5bade1813dd067fb
DrackPrez <nicoperezperez0@gmail.com>
2025-07-04 12:57:42 -0400

    Add migration to alter fields in Fase, Jornada, and SerieHonor models for improved data integrity

--- app_main/models.py
+++ app_main/models.py
@@ -45,14 +45,14 @@ class EncargadoSerie(models.Model):
         return f"{self.club} - Encargados de series"
 
 class Fase(models.Model):
-    nombre = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
 
 class Jornada(models.Model):
     fase = models.ForeignKey(Fase, on_delete=models.CASCADE, related_name='jornadas')
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.fase} - {self.nombre}"
