88639d3c41f4c9d414045012f037ee31d2ccf32a
DrackPrez <nicoperezperez0@gmail.com>
2025-07-18 10:14:19 -0400

    feat: Update STATIC_URL to include leading slash and configure STATICFILES_DIRS for additional static files

--- project_anfa/settings.py
+++ project_anfa/settings.py
@@ -140,8 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app_main', 'static'),
+    # Agrega aquí otras carpetas si tienes archivos estáticos fuera de las apps
+]
 
 # WhiteNoise para servir archivos estáticos en producción
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
